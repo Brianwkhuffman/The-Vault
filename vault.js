6bb709e5afe632c19f26c994a4b437a4334357d1
Briii <brianwkhuffman@gmail.com>
2019-07-03 17:18:21 -1000

    Done

--- vault.js
+++ vault.js
@@ -1,4 +1,25 @@
 'use strict';
 module.exports = function() {
-  
+    let theVault = {};
+
+    var setValue = function(key, value){
+        if (!key){
+            return null;
+        }else{
+            theVault[key] = value;
+        }
+    }
+
+    var getValue = function(key){
+        if (theVault.hasOwnProperty(key) === false){
+            return null;
+        }else{
+            return theVault[key];
+        }
+    }
+    return{
+        setValue: setValue,
+        getValue: getValue
+    }
+
 };
